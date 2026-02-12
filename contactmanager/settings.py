e999afe1a279ebd86b362d570036a07b62973444
adithyaaneesh <adithyaaneesh29@gmail.com>
2026-02-12 22:50:09 +0530

    connect to sql database

--- contactmanager/settings.py
+++ contactmanager/settings.py
@@ -73,12 +73,26 @@ WSGI_APPLICATION = 'contactmanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'contact',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
